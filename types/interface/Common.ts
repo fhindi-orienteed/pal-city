374f7bfbed53de0d39bdf014ca995c2d529b8ae3
waseem-jadaa <rowaid.jadaa@gmail.com>
2026-02-15 02:53:35 +0200

    feat: implement global entity schema and update common interfaces

--- types/interface/Common.ts
+++ types/interface/Common.ts
@@ -1,15 +1,27 @@
-
+export interface IGlobalProperties {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  category_id: string; // for connecting with category
+}
 export interface ILocation {
-    lat: string;
-    lng: string;
+  lat: string;
+  lng: string;
+  address: string;
 }
 
 export interface IOpeningHours {
-    friday: string;
-    monday: string;
-    sunday: string;
-    tuesday: string;
-    saturday: string;
-    thursday: string;
-    wednesday: string;
+  friday: string;
+  monday: string;
+  sunday: string;
+  tuesday: string;
+  saturday: string;
+  thursday: string;
+  wednesday: string;
+}
+// Defining the basic entity that encompasses all of the above
+export interface IBaseEntity extends IGlobalProperties {
+  location: ILocation;
+  opening_hours: IOpeningHours;
 }
